a8ee194344d9d9d12f683455bb6ec9d56127c05f
Manas Selukar <aselu@Manas-MacBook-Pro.local>
2025-11-17 10:27:22 +0530

    register route

--- skillsieve/app/register/page.js
+++ skillsieve/app/register/page.js
@@ -13,7 +13,7 @@ export default function RegisterPage() {
     setMessage("Registering...");
 
     try {
-      const res = await fetch("http://localhost:5001/api/auth/register", {
+      const res = await fetch("https://skillsieve.onrender.com/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ name, email, password }),
