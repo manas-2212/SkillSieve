7c6e6cc441bc6dec6a41d561c1e73a52d5858da1
Manas Selukar <aselu@Manas-MacBook-Pro.local>
2025-12-03 09:29:23 +0530

    code fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,48 +2,40 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { PrismaClient } from "@prisma/client";
+
 import authRoutes from "./routes/authRoutes.js";
 import skillsRoutes from "./routes/skills.routes.js";
 import internshipRoutes from "./routes/internship.routes.js";
 import testRoutes from "./routes/test.routes.js";
 
-
-
 dotenv.config();
 const app = express();
 const prisma = new PrismaClient();
-app.use("/api/test", testRoutes);
+
 
 app.use(
   cors({
     origin: [
-      "https://skillsieve.vercel.app",   // deployed frontend
-      "http://localhost:3000"           // local
+      "https://skillsieve.vercel.app", 
+      "http://localhost:3000"
     ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
 
+
 app.use(express.json());
 
 
-
-// const allowedOrigins = [
-//   "http://localhost:3000",
-//   // add your deployed frontend domain(s) here
-//   //my frontend-yet to deploy!!!!!!!!!!!,
-// ];
-
-
+app.use("/api/test", testRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/skills", skillsRoutes);
 app.use("/api/internships", internshipRoutes);
 
 
 
-// Test database connection route
 app.get("/api/test-db", async (req, res) => {
   try {
     const users = await prisma.user.findMany();
@@ -54,10 +46,13 @@ app.get("/api/test-db", async (req, res) => {
   }
 });
 
+
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
 
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
